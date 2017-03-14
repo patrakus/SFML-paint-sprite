be8471ab12ea5354dead43e160dd1b161b7dce9a
patrakus <patrakus@interia.pl>
2017-03-14 20:19:54 +0100

    Refactored code and added HSL structure

--- SFML paint sprite/Main.cpp
+++ SFML paint sprite/Main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <stdlib.h>
 #include <time.h>
+#include "HSL.h"
 
 using namespace std;
 
@@ -15,23 +16,20 @@ int main()
 		cout << "Cannot create texture" << endl;
 	}
 
-	sf::Uint8* pixels = new sf::Uint8[window.getSize().x * window.getSize().y * 4];
+	unsigned long long pixelsSize = window.getSize().x * window.getSize().y * 4;
+	sf::Uint8* pixels = new sf::Uint8[pixelsSize];
 
 	srand(time(NULL));
 
-	for (unsigned long i = 0; i < window.getSize().x * window.getSize().y * 4; i += 4)
-	{
-		pixels[i + 0] = rand() % 255 + 1;
-		pixels[i + 1] = rand() % 255 + 1;
-		pixels[i + 2] = rand() % 255 + 1;
-		pixels[i + 3] = 255;
-	}
-
-	
-	tex.update(pixels);
-
 	sf::Sprite sprite;
 	sprite.setTexture(tex);
+
+	HSL hsl;
+
+	hsl = TurnToHSL(sprite.getColor());
+	hsl.Saturation = 1;
+
+	sprite.setColor(hsl.TurnToRGB());
 
 	while (window.isOpen())
 	{
@@ -42,18 +40,20 @@ int main()
 				window.close();
 		}
 
-		window.clear();
-		window.draw(sprite);
-		window.display();
-
-		for (unsigned long i = 0; i < window.getSize().x * window.getSize().y * 4; i += 4)
+		for (unsigned long i = 0; i <pixelsSize; i += 4)
 		{
 			pixels[i + 0] = rand() % 255 + 1;
 			pixels[i + 1] = rand() % 255 + 1;
 			pixels[i + 2] = rand() % 255 + 1;
 			pixels[i + 3] = 255;
 		}
+
+
 		tex.update(pixels);
+
+		window.clear();
+		window.draw(sprite);
+		window.display();
 	}
 
 	delete pixels;
